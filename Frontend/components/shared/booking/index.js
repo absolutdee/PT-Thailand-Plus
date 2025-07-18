c8e95ee992f0fe6615878dea925ef081494d70e1
absolutdee <149473879+absolutdee@users.noreply.github.com>
2025-07-18 10:35:18 +0700

    Create index.js

---
+++ Frontend/components/shared/booking/index.js
@@ -0,0 +1,5 @@
+// Frontend/components/shared/booking/index.js
+export { default as BookingCard } from './BookingCard';
+export { default as ScheduleView } from './ScheduleView';
+export { default as TimeSlotPicker } from './TimeSlotPicker';
+export { default as SessionCard } from './SessionCard';
