108007c0a5e625d86c13969f8569689f7205045f
absolutdee <149473879+absolutdee@users.noreply.github.com>
2025-07-18 11:07:00 +0700

    Create index.js

---
+++ Frontend/src/hooks/index.js
@@ -0,0 +1,17 @@
+
+// Export all hooks
+export default {
+  useDebounce,
+  useInfiniteScroll,
+  usePagination,
+  useForm,
+  useMediaQuery,
+  useIsMobile,
+  useIsTablet,
+  useIsDesktop,
+  useOnClickOutside,
+  useLocalStorage,
+  useAsync,
+  useTimer,
+  useScrollPosition
+};
