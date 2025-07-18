89b3eca480479918ebdeaa059b537311279a48ac
absolutdee <149473879+absolutdee@users.noreply.github.com>
2025-07-18 10:24:32 +0700

    Create index.js

---
+++ Frontend/components/shared/forms/index.js
@@ -0,0 +1,8 @@
+// Frontend/components/shared/forms/index.js
+export { default as FormInput } from './FormInput';
+export { default as FormSelect } from './FormSelect';
+export { default as FormTextarea } from './FormTextarea';
+export { default as FormDatePicker } from './FormDatePicker';
+export { default as FormImageUpload } from './FormImageUpload';
+export { default as SearchBar } from './SearchBar';
+export { default as FilterPanel } from './FilterPanel';
