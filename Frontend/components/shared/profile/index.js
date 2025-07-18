972cb043d522e330265b55a6447bca8dfce1000c
absolutdee <149473879+absolutdee@users.noreply.github.com>
2025-07-18 10:45:48 +0700

    Create index.js

---
+++ Frontend/components/shared/profile/index.js
@@ -0,0 +1,5 @@
+// Frontend/components/shared/profile/index.js
+export { default as ProfileHeader } from './ProfileHeader';
+export { default as ProfileStats } from './ProfileStats';
+export { default as CertificateCard } from './CertificateCard';
+export { default as ReviewCard } from './ReviewCard';
