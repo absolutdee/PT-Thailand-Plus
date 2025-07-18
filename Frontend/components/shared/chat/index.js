3376cb50959aeef54d8495135ff8e92319695452
absolutdee <149473879+absolutdee@users.noreply.github.com>
2025-07-18 10:40:51 +0700

    Create index.js

---
+++ Frontend/components/shared/chat/index.js
@@ -0,0 +1,5 @@
+// Frontend/components/shared/chat/index.js
+export { default as ChatWindow } from './ChatWindow';
+export { default as MessageBubble } from './MessageBubble';
+export { default as ChatList } from './ChatList';
+export { default as TypingIndicator } from './TypingIndicator';
